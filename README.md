b6b422a6308ad10dd46620cb1cb860b437302a6f
tatertot365 <tate.gillespie@gmail.com>
2022-12-09 21:40:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This package contains functions for the IS 415 Data Analytics/Machine Learning class at BYU
+The functions included are
+    calculateUnivariantStatsViz 
+    calculateBivariantStatsViz
+    assumptions
+    calculateMLRandMetrics
+
+The calculateUnivariantStatsViz function only has one parameter of a pandas dataframe
+
+The rest of the functions require a pandas dataframe and a lable (y-variable)
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="test-package",
+    version="0.0.1",
+    author="Tate Gillespie",
+    author_email="tate.gillespie@gmail.com",
+    packages=["test_package"],
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
+)
